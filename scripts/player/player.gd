ab38fe892b1bd5c63967dc222b4bbe7052aa39a5
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-02-15 18:05:17 +0100

    Rename unused parameter

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 
 const SPEED = 48.0
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if direction != Vector2.ZERO:
 		velocity = direction * SPEED
