b7e1ba8ca4b2b89bba3ab28a8c4dfd6787d83a31
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-04-03 05:58:36 +0200

    Add settings WIP note

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,11 +1,12 @@
 extends Control
 
+@onready var settings: Button = $BackgroundContainer/Camera2D/CanvasLayer/ButtonContainer/Settings
 
 func _on_play_pressed() -> void:
 	SceneManager.call_scene("game")
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	settings.text = "Settings - currently WIP!"
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
